aaad374e8379a71f7c992c311788336be6585f5e
zlzksrl <zlzksrl@dingtalk.com>
2026-07-14 21:56:04 +0800

    备份

--- SoftTimer/src/SoftTimer_Maketime.h
+++ SoftTimer/src/SoftTimer_Maketime.h
@@ -1,6 +1,6 @@
 #ifndef __SoftTimer_PROJECT_MAKETIME_H__
 #define __SoftTimer_PROJECT_MAKETIME_H__
-
-#define SoftTimer_PROJECT_MAKETIME "zlzksrl-1.0.0 at placeholder"
-
+ 
+#define SoftTimer_PROJECT_MAKETIME "zlzksrl-1.0.0 at 2026.07.14-21.55.21"
+ 
 #endif
